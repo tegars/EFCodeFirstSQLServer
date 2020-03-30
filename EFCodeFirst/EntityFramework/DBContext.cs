d8ff0d6bef314e35236ad3fd9ea37bb01ce159bf
tegarsweefer <tegar.s@weefer.co.id>
2020-03-30 10:48:08 +0700

    first commit

---
+++ EFCodeFirst/EntityFramework/DBContext.cs
@@ -0,0 +1,46 @@
+﻿using FirstEF.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstEF.EntityFramework
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=localhost;database=FirstEF;user=sa;password=aaaa123");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasIndex(c => c.Id);
+            modelBuilder.Entity<Product>().HasIndex(c => c.Id);
+            modelBuilder.Entity<Category>()
+                .HasMany<Product>(g => g.Products)
+                .WithOne(s => s.Category)
+                .HasForeignKey(s => s.CategoryId);
+
+            
+            Category category = new Category();
+            category.Id = 1;
+            category.Name = "Fashion Wanita";
+            modelBuilder.Entity<Category>().HasData(category);
+
+            Category category2 = new Category();
+            category2.Id = 2;
+            category2.Name = "Fashion Pria";
+            modelBuilder.Entity<Category>().HasData(category2);
+
+            Category category3 = new Category();
+            category3.Id = 3;
+            category3.Name = "Handphone";
+            modelBuilder.Entity<Category>().HasData(category3);
+        }
+    }
+}
---
+++ EFCodeFirst/Migrations/20200330034155_initialTable.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCodeFirst.Migrations
+{
+    public partial class initialTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Colour = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Fashion Wanita" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Fashion Pria" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Handphone" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Id",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Id",
+                table: "Products",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
